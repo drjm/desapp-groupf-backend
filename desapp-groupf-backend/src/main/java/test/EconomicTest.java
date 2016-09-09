3a00c85ed76b6b64dabf20828a655a78dcbaf51c
avalospablod <avalospablod@gmail.com>
2016-09-09 01:13:55 -0300

    JUnit version modified

--- desapp-groupf-backend/src/main/java/test/EconomicTest.java
+++ desapp-groupf-backend/src/main/java/test/EconomicTest.java
@@ -1,7 +1,10 @@
 package test;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
 import org.junit.Test;
+//import org.junit.Test;
 import org.mockito.Mockito;
 
 import model.Profile;
