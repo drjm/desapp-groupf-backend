8098d6bd472aaaed7a769339fbe6766606e587c3
meportillo <mauroeportillo@gmail.com>
2016-09-10 08:34:40 -0300

    filtro salidas con amigos imple

--- desapp-groupf-backend/src/main/java/model/WithFriends.java
+++ desapp-groupf-backend/src/main/java/model/WithFriends.java
@@ -1,12 +1,34 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WithFriends extends Filter {
 
-	
 	@Override
-	public Boolean applyFilter(Event e, Object profile) {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean applyFilter(Event e, Object usersParam) {
+
+		List<User> users = (ArrayList<User>) usersParam;
+
+		Boolean isCompatible = true;
+
+		for (User user : users) {
+			isCompatible = isCompatible && e.isCompatible(user.getProfile());
+		}
+		return isCompatible;
+	}
+
+	public List<Event> filter(List<Event> listEvent, List<User> users) {
+
+		List<Event> eventSuggestion = new ArrayList<Event>();
+
+		for (Event e : listEvent) {
+
+			if (this.applyFilter(e, users)) {
+				eventSuggestion.add(e);
+			}
+		}
+		return eventSuggestion;
 	}
 
 }
