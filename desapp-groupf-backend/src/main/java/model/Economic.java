504e56b0ce1a5d29a32aa98088e4679627b8cc77
avalospablod <avalospablod@gmail.com>
2016-09-11 15:41:15 -0300

    Economic Fits filter

--- desapp-groupf-backend/src/main/java/model/Economic.java
+++ desapp-groupf-backend/src/main/java/model/Economic.java
@@ -1,25 +1,10 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Economic extends Filter {
 
 	@Override
-	public List<Event> filter(List<Event> listEvent, User user) {
-		List<Event> eventToAmountMaxOfUser = new ArrayList<Event>(); 
-		for(Event event:listEvent){
-			if(event.getPrice() <= user.getProfile().getAmuntMax()){
-				eventToAmountMaxOfUser.add(event);
-			}
-		}
-		return eventToAmountMaxOfUser;
-	}
-
-	@Override
-	public Boolean applyFilter(Event e, Object profile) {
-		// TODO Auto-generated method stub
-		return null;
+	public Boolean applyFilter(Event e, Object user) {
+		return e.getPrice() <= ((User)user).getProfile().getAmuntMax();
 	}
 
 }
